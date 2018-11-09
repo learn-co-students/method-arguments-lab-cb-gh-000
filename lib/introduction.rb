40c4f4c32f211d0c058e80fee82fa1a199267224
Joseph Corona <josed.hc96@gmail.com>
2018-11-09 22:04:50 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(str)
+
+  puts "Hi, my name is #{str}."
+end
+
+def introduction_with_language (str1, str2)
+
+  puts "Hi, my name is #{str1} and I am learning to program in #{str2}."
+end
