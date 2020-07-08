a07f8168c07689c15ec05c36e7e02f59ed8e3052
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 13:27:09 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,6 @@
-def introduction (name, language)
-  puts "Hi, my name is #{name} and I am
-  learning to program in #{language}."
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
